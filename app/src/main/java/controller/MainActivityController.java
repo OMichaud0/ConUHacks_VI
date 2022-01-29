1e0cdf207b25e86be1bf4ed0ba432114ff68bbad
OMichaud0 <98564154+OMichaud0@users.noreply.github.com>
2022-01-29 14:29:44 -0500

    Added LoginActivity

--- app/src/main/java/controller/MainActivityController.java
+++ app/src/main/java/controller/MainActivityController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import static com.spotify.sdk.android.auth.AccountsQueryParameters.CLIENT_ID;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.conuhacks_vi.R;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
 public class MainActivityController extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,5 +27,39 @@ public class MainActivityController extends AppCompatActivity {
 
     public void generatePlaylist(View view){
         startActivity(new Intent(this, GeneratePlaylistController.class));
+
+
+        AuthorizationRequest.Builder builder =
+                new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthorizationRequest request = builder.build();
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
     }
 }
